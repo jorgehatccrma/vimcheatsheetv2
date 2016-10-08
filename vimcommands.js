88d67ff6da5dc942d64972f01612d727681e54c4
Jorge Herrera <jorge.herrera@shazam.com>
2016-10-07 23:47:01 -0700

    added commands

--- vimcommands.js
+++ vimcommands.js
@@ -19,14 +19,14 @@ const data = {
 		{char: "s", desc: "sentence"},
 		{char: "p", desc: "paragraph"},
 		{char: "b", desc: "block (parentheses)"},
-		{char: "t", desc: "tag (e.g. <div>"}
+		{char: "t", desc: "tag (e.g. <div>)"}
 	],
 	examples: [
-	{cmd: "dw", desc: "delete word", fulldesc: "from the cursor's location till the end of the word"},
-	{cmd: "diw", desc: "delete inside word", fulldesc: "the whole word, regardless of where in the word the cursor is"},
-	{cmd: "ciw", desc: "change inside word", fulldesc: "delete the current word and enter insert mode immediately"},
-	{cmd: "vas", desc: "visual around sentence", fulldesc: "visually select the whole sentence"},
-	{cmd: "yf)", desc: "yank till )", fulldesc: "copy fomr the cursor's position until the first occurrence of ')'"}
+		{cmd: "dw", desc: "delete word", fulldesc: "from the cursor's location till the end of the word"},
+		{cmd: "diw", desc: "delete inside word", fulldesc: "the whole word, regardless of where in the word the cursor is"},
+		{cmd: "ciw", desc: "change inside word", fulldesc: "delete the current word and enter insert mode immediately"},
+		{cmd: "vas", desc: "visual around sentence", fulldesc: "visually select the whole sentence"},
+		{cmd: "yf)", desc: "yank till )", fulldesc: "copy fomr the cursor's position until the first occurrence of ')'"}
 	]
 };
 
@@ -49,24 +49,26 @@ const displayCommands = function(container) {
 			nouns = body.append("td").append("ul").selectAll("li").data(data.nouns);
 
 	[verbs, modifiers, nouns].forEach(function(sel) {
-		sel.enter()
-				.append("li")
-				.html(function(d,i) {
-					return "<b>" + d.char + "</b>: " + d.desc;
-				});
+		let li = sel.enter()
+								.append("li");
+		li.append("code").text(function(d,i) { return d.char; });
+		li.append("span").text(function(d,i) { return d.desc; });
 	});
 
 	container.append("h2").html("Examples");
 
-	let t2 = container.append("table").classed("examples", true);
+	let t2 = container.append("table")
+										.classed("examples", true)
+										.classed("pure-table", true)
+										.classed("pure-table-striped", true);
 
 	let examples = t2.selectAll("tr").data(data.examples);
 	examples.enter()
 					.append("tr")
 					.each(function(d,i) {
 						var s = d3.select(this);
-						s.append("th").html(d.cmd);
-						s.append("td").html(d.desc);
-						s.append("td").html(d.fulldesc);
+						s.append("th").text(d.cmd);
+						s.append("td").text(d.desc);
+						s.append("td").text(d.fulldesc);
 					});
 };
